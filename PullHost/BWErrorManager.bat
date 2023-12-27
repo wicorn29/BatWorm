68c81b34961b3549dbfd77baf7c9910562c9bd6f
Wil Corn <wilson.corn@hotmail.com>
2023-12-27 10:51:26 -0500

    Update BWErrorManager.bat

--- PullHost/BWErrorManager.bat
+++ PullHost/BWErrorManager.bat
@@ -2,7 +2,7 @@
 title Windows System Startup Manager
 set version=1.0
 
-echo %version%
+REM echo %version%
 
 powershell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Unfortunately, wmimgmt.msc Has Stopped','Program Malfunction')}" && del BWErrorManager.bat
-pause
+echo %version%
